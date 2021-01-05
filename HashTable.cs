535f5694cd4875deec7a614ab3a311c0acafec5e
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2021-01-05 11:44:41 +0100

    Implementing IDictionary interface explicitly

--- HashTable.cs
+++ HashTable.cs
@@ -17,19 +17,6 @@ namespace HashMap
         private int _arraySize;
         private T[] keys, values;
 
-        int ICollection<T>.Count => _count;
-
-        bool ICollection<T>.IsReadOnly => false;
-
-        ICollection<T> IDictionary<T, T1>.Keys => throw new NotImplementedException();
-
-        ICollection<T1> IDictionary<T, T1>.Values => throw new NotImplementedException();
-
-        int ICollection<KeyValuePair<T, T1>>.Count => throw new NotImplementedException();
-
-        bool ICollection<KeyValuePair<T, T1>>.IsReadOnly => throw new NotImplementedException();
-
-        T1 IDictionary<T, T1>.this[T key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public HashTable() : this(127)
         {
@@ -42,6 +29,56 @@ namespace HashMap
 
             keys = new T[_arraySize];
             values = new T[_arraySize];
+        }
+
+        T1 IDictionary<T, T1>.this[T key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        ICollection<T> IDictionary<T, T1>.Keys => throw new NotImplementedException();
+
+        ICollection<T1> IDictionary<T, T1>.Values => throw new NotImplementedException();
+
+        int ICollection<KeyValuePair<T, T1>>.Count => throw new NotImplementedException();
+
+        bool ICollection<KeyValuePair<T, T1>>.IsReadOnly => throw new NotImplementedException();
+
+        void IDictionary<T, T1>.Add(T key, T1 value)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<KeyValuePair<T, T1>>.Add(KeyValuePair<T, T1> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<KeyValuePair<T, T1>>.Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<KeyValuePair<T, T1>>.Contains(KeyValuePair<T, T1> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDictionary<T, T1>.ContainsKey(T key)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<KeyValuePair<T, T1>>.CopyTo(KeyValuePair<T, T1>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<KeyValuePair<T, T1>> IEnumerable<KeyValuePair<T, T1>>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
 
         int nextPrimeNumber(int current)
@@ -74,77 +111,7 @@ namespace HashMap
             return Math.Abs(BitConverter.ToInt32(hash, 0) % _arraySize);
         }
 
-        void ICollection<T>.Add(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        void ICollection<T>.Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        bool ICollection<T>.Contains(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        void ICollection<T>.CopyTo(T[] array, int arrayIndex)
-        {
-            throw new NotImplementedException();
-        }
-
-        bool ICollection<T>.Remove(T item)
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator<T> IEnumerable<T>.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
-
-        void IDictionary<T, T1>.Add(T key, T1 value)
-        {
-            throw new NotImplementedException();
-        }
-
-        bool IDictionary<T, T1>.ContainsKey(T key)
-        {
-            throw new NotImplementedException();
-        }
-
         bool IDictionary<T, T1>.Remove(T key)
-        {
-            throw new NotImplementedException();
-        }
-
-        bool IDictionary<T, T1>.TryGetValue(T key, out T1 value)
-        {
-            throw new NotImplementedException();
-        }
-
-        void ICollection<KeyValuePair<T, T1>>.Add(KeyValuePair<T, T1> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        void ICollection<KeyValuePair<T, T1>>.Clear()
-        {
-            throw new NotImplementedException();
-        }
-
-        bool ICollection<KeyValuePair<T, T1>>.Contains(KeyValuePair<T, T1> item)
-        {
-            throw new NotImplementedException();
-        }
-
-        void ICollection<KeyValuePair<T, T1>>.CopyTo(KeyValuePair<T, T1>[] array, int arrayIndex)
         {
             throw new NotImplementedException();
         }
@@ -154,7 +121,7 @@ namespace HashMap
             throw new NotImplementedException();
         }
 
-        IEnumerator<KeyValuePair<T, T1>> IEnumerable<KeyValuePair<T, T1>>.GetEnumerator()
+        bool IDictionary<T, T1>.TryGetValue(T key, out T1 value)
         {
             throw new NotImplementedException();
         }
