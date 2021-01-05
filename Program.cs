ab394eca3340181063bea9e63ed2338ecd5cbfe2
Elmassaoudi Yassine <Massaoudi.yass@gmail.com>
2021-01-05 19:45:38 +0100

    wrote some tests

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Security.Cryptography;
 
@@ -9,10 +10,35 @@ namespace HashMap
     {
         static void Main(string[] args)
         {
-            Dictionary<int, int> dic = new();
-            HashTable<string, string> table = new();
+            bool test = true;
+            int count = 10000;
 
+            Stopwatch watch = new();
+            watch.Start();
+            HashTable<int, string> t = new(count * 2);
+            for (int n = 0; n < count; n++)
+            {
+                t.Add(n, $"Y{n}");
+            }
+            watch.Stop();
 
+            while (true)
+            {
+                HashTable<int, string> table = new();
+                foreach (var n in Enumerable.Range(0, count))
+                {
+                    table.Add(n, $"Y{n}");
+                }
+
+                foreach (var n in Enumerable.Range(0, count))
+                {
+                    test &= table[n] == $"Y{n}";
+                }
+
+                count *= 2;
+                if (!test || count > 10_000_000)
+                    break;
+            }
         }
     }
 }
